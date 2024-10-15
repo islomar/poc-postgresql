0ca0445d69f5a9f784b689082ccf66f30634a883
Isidro Lopez <islomar@gmail.com>
2024-10-15 19:17:10 +0200

    Fix populating the database

--- init_sql_scripts/create_tables.sql
+++ init_sql_scripts/create_tables.sql
@@ -1,37 +1,37 @@
 -- Creation of product table
 CREATE TABLE IF NOT EXISTS product (
-                                       product_id INT NOT NULL,
-                                       name varchar(250) NOT NULL,
-                                       PRIMARY KEY (product_id)
+    product_id INT NOT NULL,
+    name varchar(250) NOT NULL,
+    PRIMARY KEY (product_id)
 );
 
 -- Creation of country table
 CREATE TABLE IF NOT EXISTS country (
-                                       country_id INT NOT NULL,
-                                       country_name varchar(450) NOT NULL,
-                                       PRIMARY KEY (country_id)
+    country_id INT NOT NULL,
+    country_name varchar(450) NOT NULL,
+    PRIMARY KEY (country_id)
 );
 
 -- Creation of city table
 CREATE TABLE IF NOT EXISTS city (
-                                    city_id INT NOT NULL,
-                                    city_name varchar(450) NOT NULL,
-                                    country_id INT NOT NULL,
-                                    PRIMARY KEY (city_id),
-                                    CONSTRAINT fk_country
-                                        FOREIGN KEY(country_id)
-                                            REFERENCES country(country_id)
+    city_id INT NOT NULL,
+    city_name varchar(450) NOT NULL,
+    country_id INT NOT NULL,
+    PRIMARY KEY (city_id),
+    CONSTRAINT fk_country
+    FOREIGN KEY(country_id)
+        REFERENCES country(country_id)
 );
 
 -- Creation of store table
 CREATE TABLE IF NOT EXISTS store (
-                                     store_id INT NOT NULL,
-                                     name varchar(250) NOT NULL,
-                                     city_id INT NOT NULL,
-                                     PRIMARY KEY (store_id),
-                                     CONSTRAINT fk_city
-                                         FOREIGN KEY(city_id)
-                                             REFERENCES city(city_id)
+    store_id INT NOT NULL,
+    name varchar(250) NOT NULL,
+    city_id INT NOT NULL,
+    PRIMARY KEY (store_id),
+    CONSTRAINT fk_city
+     FOREIGN KEY(city_id)
+         REFERENCES city(city_id)
 );
 
 -- Creation of user table
